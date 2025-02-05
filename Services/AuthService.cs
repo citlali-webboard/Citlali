3822bcd89c3344682ecbc23752106fe8da603106
PpoH <2ndPipatpong987123@gmail.com>
2025-02-05 11:55:11 +0700

    ✨ add error handling in AuthController  (Sign in, Sign up, Confirm OTP)

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -2,8 +2,9 @@ using Supabase;
 using DotNetEnv;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-
+using System.Text.Json;
 using Citlali.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Citlali.Services;
 
@@ -12,18 +13,44 @@ public class AuthService(Client supabaseClient, IConfiguration configuration)
     private readonly Client _supabaseClient = supabaseClient;
     private readonly IConfiguration _configuration = configuration;
 
-    public async Task<Supabase.Gotrue.Session?> SignIn(string email, string password) {
-        var response = await _supabaseClient.Auth.SignIn(email, password);
-        return response;
+    public async Task<Supabase.Gotrue.Session?> SignIn(string email, string password){
+        try{
+            var response = await _supabaseClient.Auth.SignIn(email, password);
+            return response;
+
+        }catch(Exception e){
+            
+            var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
+            string msgError = errorJson.GetProperty("msg").GetString()??"";
+            Console.WriteLine(msgError);
+
+            throw new Exception(msgError);
+        }
     }
 
-    public async Task<Supabase.Gotrue.Session?> SignUp(string email, string password) {
-        var response = await _supabaseClient.Auth.SignUp(email, password);
-        return response;
+    public async Task<Supabase.Gotrue.Session?> SignUp(string email, string password){
+        try{
+            var response = await _supabaseClient.Auth.SignUp(email, password);
+            return response;
+        }catch(Exception e){
+            var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
+            string msgError = errorJson.GetProperty("msg").GetString()??"";
+            Console.WriteLine(msgError);
+
+            throw new Exception(msgError);
+        }
     }
 
     public async Task<Supabase.Gotrue.Session?> VerifyEmailOtp(string email, string token, Supabase.Gotrue.Constants.EmailOtpType type) {
-        var response = await _supabaseClient.Auth.VerifyOTP(email, token, type);
-        return response;
+        try{
+            var response = await _supabaseClient.Auth.VerifyOTP(email, token, type);
+            return response;
+        }catch(Exception e){
+            var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
+            string msgError = errorJson.GetProperty("msg").GetString()??"";
+            Console.WriteLine(msgError);
+
+            throw new Exception(msgError);
+        }
     }
 }
