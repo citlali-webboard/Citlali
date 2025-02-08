a323cd1f0c13f3d0c94e5c1bee91e3b555900a4d
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-02-08 15:56:39 +0700

    move detail card to a component

--- Models/EventModel.cs
+++ Models/EventModel.cs
@@ -34,7 +34,7 @@ public class EventLocationTag {
     public string EventLocationTagName = "Lat Krabang";
 }
 
-public class EventDetailViewModel {
+public class EventDetailCardData {
     public Guid EventId = new();
     public string EventTitle = "Basketball? Anyone?";
     public string EventDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -47,7 +47,10 @@ public class EventDetailViewModel {
     public DateTime EventDate = new(2024, 12, 31);
     public DateTime PostExpiryDate = new(2024, 12, 30);
     public DateTime CreatedAt = new(2024, 12, 3);
+}
 
+public class EventDetailViewModel {
+    public EventDetailCardData EventDetailCardData = new();
     public List<QuestionViewModel> Questions { get; set; } = [new()];
 }
 
