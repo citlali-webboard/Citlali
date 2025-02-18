ee5e1fddecd3b10023d6c8e863f53dc35927a004
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-02-18 12:50:01 +0700

    fix: :bug: set user to null when cookie is not found

--- Program.cs
+++ Program.cs
@@ -50,11 +50,14 @@ builder.Services.AddAuthentication()
                         {
                             var accessToken = context.Request.Cookies[configuration.Jwt.AccessCookie];
                             var refreshToken = context.Request.Cookies[configuration.Jwt.RefreshCookie];
+                            var userService = context.HttpContext.RequestServices.GetRequiredService<UserService>();
                             if (!string.IsNullOrEmpty(accessToken) && !string.IsNullOrEmpty(refreshToken)) {
                                 context.Token = accessToken;
-                                await supabaseClient.Auth.SetSession(accessToken, refreshToken);
+                                userService.CurrentSession = await supabaseClient.Auth.SetSession(accessToken, refreshToken);
                                 // await supabaseClient.Auth.RefreshSession();
                                 // return Task.CompletedTask;
+                            } else {
+                                userService.CurrentSession.User = null;
                             }
                         },
                         OnAuthenticationFailed = context =>
