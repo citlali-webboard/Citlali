1a1ce50c518a94ac8e6d10fd739b2438e29f9761
PpoH <2ndPipatpong987123@gmail.com>
2025-02-20 13:56:22 +0700

    feat ✨ able to display detail on View and mark as read

--- wwwroot/js/Notification/notification.js
+++ wwwroot/js/Notification/notification.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
             let xmlhttp = new XMLHttpRequest();
 
             //self + notification/detail/ + CardId
-            let URL = window.location.href + "detail/" + CardId;
+            let URL = window.location.href + "/detail/" + CardId;
             console.log(URL);
             xmlhttp.open("GET", URL , true);
 
@@ -22,6 +22,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { // readyState 4 means the operation is complete and status 200 means the request is successful
                     let data = JSON.parse(xmlhttp.responseText);
                     console.log(data);
+
+                    // set details 
+                    document.querySelector("#content-title").innerHTML = data.title;
+                    document.querySelector("#content-body").innerHTML = data.message;
+                    document.querySelector("#source-name").innerHTML = `${data.sourceDisplayName}(${data.sourceUsername})`
+                    document.querySelector("#create-at").href = data.createdAt;
+                    let source_img = document.querySelector("#source-img");
+                    source_img.src = data.sourceProfileImageUrl; 
+
+                    //set the card as read and cannot hover
+                    card.classList.add("read");
+
+                    //remove hidden from detail 
+                    document.querySelector("#notification-detail").classList.remove("hidden");
+                    
+                    //set default-notification hidden
+                    document.querySelector("#default-notification").classList.add("hidden");
                 }
             }
             xmlhttp.send();
