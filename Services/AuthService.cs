fc1ab4f7230e61dc6c715f208027fc4ec75a5ac5
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-02-21 23:07:27 +0700

    feat: :sparkles: try catch deserializing json where gotrue exception is can be either string or just text

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -9,74 +9,130 @@ public class AuthService(Supabase.Client supabaseClient, IConfiguration configur
     private readonly Supabase.Client _supabaseClient = supabaseClient;
     private readonly IConfiguration _configuration = configuration;
 
-    public async Task<Session?> SignIn(string email, string password){
-        try{
+    public async Task<Session?> SignIn(string email, string password)
+    {
+        try
+        {
             var response = await _supabaseClient.Auth.SignIn(email, password);
             return response;
 
-        }catch(Exception e){
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
+                string msgError = errorJson.GetProperty("msg").GetString() ?? "";
+                Console.WriteLine(msgError);
 
-            var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
-            string msgError = errorJson.GetProperty("msg").GetString()??"";
-            Console.WriteLine(msgError);
-
-            throw new Exception(msgError);
+                throw new Exception(msgError);
+            }
+            catch
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 
-    public async Task<Session?> SignUp(string email, string password){
-        try{
+    public async Task<Session?> SignUp(string email, string password)
+    {
+        try
+        {
             var response = await _supabaseClient.Auth.SignUp(email, password);
             return response;
-        }catch(Exception e){
-            var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
-            string msgError = errorJson.GetProperty("msg").GetString()??"";
-            Console.WriteLine(msgError);
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
+                string msgError = errorJson.GetProperty("msg").GetString() ?? "";
+                Console.WriteLine(msgError);
 
-            throw new Exception(msgError);
+                throw new Exception(msgError);
+            }
+            catch
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 
-    public async Task<ResetPasswordForEmailState> ForgotPassword(string email){
-        try{
-            var response = await _supabaseClient.Auth.ResetPasswordForEmail(new ResetPasswordForEmailOptions(email) {
+    public async Task<ResetPasswordForEmailState> ForgotPassword(string email)
+    {
+        try
+        {
+            var response = await _supabaseClient.Auth.ResetPasswordForEmail(new ResetPasswordForEmailOptions(email)
+            {
                 RedirectTo = "/auth/reset-password"
             });
             return response;
-        }catch(Exception e){
-            var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
-            string msgError = errorJson.GetProperty("msg").GetString()??"";
-            Console.WriteLine(msgError);
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
+                string msgError = errorJson.GetProperty("msg").GetString() ?? "";
+                Console.WriteLine(msgError);
 
-            throw new Exception(msgError);
+                throw new Exception(msgError);
+            }
+            catch
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 
-    public async Task<User?> ResetPassword(string password){
-        try{
-            var response = await _supabaseClient.Auth.Update(new UserAttributes {
+    public async Task<User?> ResetPassword(string password)
+    {
+        try
+        {
+            var response = await _supabaseClient.Auth.Update(new UserAttributes
+            {
                 Password = password
             });
             return response;
-        }catch(Exception e){
-            var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
-            string msgError = errorJson.GetProperty("msg").GetString()??"";
-            Console.WriteLine(msgError);
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
+                string msgError = errorJson.GetProperty("msg").GetString() ?? "";
+                Console.WriteLine(msgError);
 
-            throw new Exception(msgError);
+                throw new Exception(msgError);
+            }
+            catch
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 
-    public async Task<Session?> VerifyEmailOtp(string email, string token, Constants.EmailOtpType type) {
-        try{
+    public async Task<Session?> VerifyEmailOtp(string email, string token, Constants.EmailOtpType type)
+    {
+        try
+        {
             var response = await _supabaseClient.Auth.VerifyOTP(email, token, type);
             return response;
-        }catch(Exception e){
-            var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
-            string msgError = errorJson.GetProperty("msg").GetString()??"";
-            Console.WriteLine(msgError);
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                var errorJson = JsonSerializer.Deserialize<JsonElement>(e.Message);
+                string msgError = errorJson.GetProperty("msg").GetString() ?? "";
+                Console.WriteLine(msgError);
 
-            throw new Exception(msgError);
+                throw new Exception(msgError);
+            }
+            catch
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 }
